be2acd7110da492a965f626b20179b273da47f67
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-10-27 19:44:49 +0300

    Add simple memory, use rustfmt, add 4 numbers
    Add memory, position and store operation in it.
    
    Add an example of adding 4 numbers.
    
    Use rustfmt.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 struct CPU {
-    current_operation: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
 }
 
 impl CPU {
     fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let p = self.position_in_memory;
+        let op_byte1 = self.memory[p] as u16;
+        let op_byte2 = self.memory[p + 1] as u16;
+
+        // To create a u16 opcode we combine two values from memory
+        // with the logical OR operation.
+        // They need to be cast as u16 to start with, otherwise the left-shift
+        // will set all the bits to 0.
+        op_byte1 << 8 | op_byte2
     }
 
     fn run(&mut self) {
-        // loop {
+        loop {
             let opcode = self.read_opcode();
+
+            // Increment position in memory to point to the next instruction.
+            self.position_in_memory += 2;
 
             let c = ((opcode & 0xF000) >> 12) as u8;
             let x = ((opcode & 0x0F00) >> 8) as u8;
@@ -18,14 +30,28 @@ impl CPU {
             let d = ((opcode & 0x000F) >> 0) as u8;
 
             match (c, x, y, d) {
+                // Opcode x0000 means stop.
+                (0, 0, 0, 0) => return,
                 (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
-        // }
+        }
     }
 
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+
+        let (val, overflow_detected) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+
+        if overflow_detected {
+            self.registers[0xF] = 1;
+
+            return;
+        }
+
+        self.registers[0xF] = 0;
     }
 }
 
@@ -49,17 +75,31 @@ fn main() {
     // assert_eq!(kk, 0xE4);
 
     let mut cpu = CPU {
-        current_operation: 0,
-        registers: [0; 2],
+        registers: [0; 16],
+        memory: [0; 4096],
+        position_in_memory: 0,
     };
 
-    cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+
+    // 0x8014 - add register 1 to register 0.
+    cpu.memory[0] = 0x80;
+    cpu.memory[1] = 0x14;
+
+    // 0x8024 - add register 2 to register 0.
+    cpu.memory[2] = 0x80;
+    cpu.memory[3] = 0x24;
+
+    // 0x8034 - add register 3 to register 0.
+    cpu.memory[4] = 0x80;
+    cpu.memory[5] = 0x34;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 15);
+    assert_eq!(cpu.registers[0], 35);
 
-    println!("5 + 10 = {}", cpu.registers[0]);
+    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
 }
