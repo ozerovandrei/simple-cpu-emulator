bd76ba287abf65bb5ee3ef2471dc7d0d69ccc459
Andrei Ozerov <andrei.ozerov92@gmail.com>
2020-10-27 19:17:12 +0300

    Add simple tests inside main()
    Unit tests will be implemented later.

--- src/main.rs
+++ src/main.rs
@@ -30,12 +30,21 @@ impl CPU {
 }
 
 fn main() {
-    let mut cpu = CPU{
-        current_operation: 0,
-        registers: [0; 2],
-    };
+    let opcode: u16 = 0x71E4;
 
-    cpu.current_operation = 0x8014;
-    cpu.registers[0] = 5;
-    cpu.registers[1] = 10;
+    let c = ((opcode & 0xF000) >> 12) as u8;
+    let x = ((opcode & 0x0F00) >> 8) as u8;
+    let y = ((opcode & 0x00F0) >> 4) as u8;
+    let d = ((opcode & 0x000F) >> 0) as u8;
+
+    assert_eq!(c, 0x7);
+    assert_eq!(x, 0x1);
+    assert_eq!(y, 0xE);
+    assert_eq!(d, 0x4);
+
+    let nnn = opcode & 0x0FFF;
+    let kk = opcode & 0x00FF;
+
+    assert_eq!(nnn, 0x1E4);
+    assert_eq!(kk, 0xE4);
 }
